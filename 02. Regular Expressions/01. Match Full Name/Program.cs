666d697e7e831e1d52084f315d2f0c7003978b77
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:27:56 +0200

    Update Program.cs

--- 02. Regular Expressions/01. Match Full Name/Program.cs
+++ 02. Regular Expressions/01. Match Full Name/Program.cs
@@ -1,1 +1,11 @@
+using System.Text.RegularExpressions;
 
+string input = Console.ReadLine();
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+Console.WriteLine(string.Join(" ", matches));
