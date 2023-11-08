8a5c02ae03fea2c5ed1e2a9fadd910c3b79c2603
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:18:39 +0200

    Add files via upload

---
+++ 01. Strings and Text Processing/01. Reverse Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+
+while (input != "end")
+{
+    Console.WriteLine(input + " = " + Reverse(input));
+
+    input = Console.ReadLine();
+}
+
+static string Reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
