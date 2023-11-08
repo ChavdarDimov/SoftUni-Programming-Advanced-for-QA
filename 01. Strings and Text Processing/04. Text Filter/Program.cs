b9b9fa711cb425ad1473e5dc453f6727232eb99f
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:22:45 +0200

    Update Program.cs

--- 01. Strings and Text Processing/04. Text Filter/Program.cs
+++ 01. Strings and Text Processing/04. Text Filter/Program.cs
@@ -1,1 +1,13 @@
+string[] banWOrds = Console.ReadLine().Split(", ");
 
+string text = Console.ReadLine();
+
+foreach (string banWord in banWOrds)
+{
+    if (text.Contains(banWord))
+    {
+        text = text.Replace(banWord, new string('*', banWord.Length));
+    }
+}
+
+Console.WriteLine(text);
