6807dc20d5ada3dae2c2531559cd895e961ef44e
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:29:05 +0200

    Update Program.cs

--- 02. Regular Expressions/02. Match Phone Number/Program.cs
+++ 02. Regular Expressions/02. Match Phone Number/Program.cs
@@ -1,1 +1,11 @@
+using System.Text.RegularExpressions;
 
+string input = Console.ReadLine();
+
+string pattern = @"\+359([\ \-])2\1\d{3}\1\d{4}\b";
+
+Regex regex = new Regex(pattern);
+
+MatchCollection matches = regex.Matches(input);
+
+Console.WriteLine(string.Join(", ", matches));
