151e9d8b4e77c5f096901167e9e7e2ee3cbe9f35
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:21:08 +0200

    Update Program.cs

--- 01. Strings and Text Processing/02. Repeat Strings/Program.cs
+++ 01. Strings and Text Processing/02. Repeat Strings/Program.cs
@@ -1,1 +1,16 @@
+string[] arrayOfStrings = Console.ReadLine().Split();
+
+string result = "";
+
+foreach (string word in arrayOfStrings)
+{
+    int repeatCoutner = word.Length;
+
+    for (int i = 0; i < repeatCoutner; i++)
+    {
+        result = result + word;
+    }
+}
+
+Console.WriteLine(result);
 
