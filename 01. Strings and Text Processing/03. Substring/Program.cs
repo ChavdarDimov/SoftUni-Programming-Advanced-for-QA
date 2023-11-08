fcedd361eb6231bd7001f58fded88066292cd622
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-08 21:21:54 +0200

    Update Program.cs

--- 01. Strings and Text Processing/03. Substring/Program.cs
+++ 01. Strings and Text Processing/03. Substring/Program.cs
@@ -1,1 +1,13 @@
+string wordToRemove = Console.ReadLine();
+string text = Console.ReadLine();
 
+int index = text.IndexOf(wordToRemove);
+
+while (index != -1)
+{
+    text = text.Remove(index, wordToRemove.Length);
+
+    index = text.IndexOf(wordToRemove);
+}
+
+Console.WriteLine(text);
