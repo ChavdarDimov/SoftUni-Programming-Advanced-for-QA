e6a30a947421be4e98f59ff31d4aa60d8acb4c2b
Chavdar Dimov <125730670+ChavdarDimov@users.noreply.github.com>
2023-11-20 17:14:15 +0200

    Create program.cs

---
+++ 04. Dictionaries, Lambda and LINQ/03. Word Synonyms/program.cs
@@ -0,0 +1,24 @@
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (!synonyms.ContainsKey(word))
+    {
+        synonyms[word] = new List<string>();
+    }
+
+    synonyms[word].Add(synonym);
+}
+
+foreach (var kvp in synonyms)
+{
+    string word = kvp.Key;
+    List<string> synonymList = kvp.Value;
+
+    Console.WriteLine($"{word} - {string.Join(", ", synonymList)}");
+}
